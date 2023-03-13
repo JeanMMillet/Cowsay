3f65b820c71a4bfcff1e41c859464393f4c55411
JeanMMillet <millet.jm@live.com>
2023-03-13 15:55:44 +0100

    Finish exercice

---
+++ index.js
@@ -0,0 +1,9 @@
+const myInfo = require('./information');
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text: `Hello, I'm ${myInfo.name} from ${myInfo.campus}`,
+    e: "XX",
+    T: "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const myInfos = {
+    name: 'Jean-Marc',
+    campus: 'Paris'
+}
+
+module.exports = {
+    name: myInfos.name,
+    campus: myInfos.campus
+}
